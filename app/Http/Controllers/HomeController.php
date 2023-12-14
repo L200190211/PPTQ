b96e8d4767bd519f2d1ab3b905bd9ddce95ed0bb
Sherly Maycana <sherlymayca26@gmail.com>
2023-12-14 15:13:52 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -23,7 +27,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.beranda');
+        $aktif = User::where('status', 'Active')->where(
+            function ($query) {
+                return $query->where('admin', 'user');
+            }
+        )->count();
+        $pause = User::where('status', 'Pause')->where(
+            function ($query) {
+                return $query->where('admin', 'user');
+            }
+        )->count();
+        return view('admin.beranda', compact('aktif', 'pause'));
     }
-
 }

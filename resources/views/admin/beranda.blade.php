939e359cc5ac2933bd7961a3c803ac08adf01f50
Sherly Maycana <sherlymayca26@gmail.com>
2023-12-14 15:24:48 +0700

    upt

--- resources/views/admin/beranda.blade.php
+++ resources/views/admin/beranda.blade.php
@@ -69,6 +69,26 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Unordered List</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum:</p>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</li>
+                            <li>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</li>
+                            <li>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</li>
+                            <li>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</li>
+                            <li>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</li>
+                            <li>proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
 @endsection
